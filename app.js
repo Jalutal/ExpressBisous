af83824596464cb82f7bd59c3fb6ebc3e4f5ffff
Jesse Granier <jesse.granier@lapiscine.pro>
2023-11-28 13:41:38 +0100

    AVANT QUE CHLOE LE VOIT

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const port = 3000
 // Port arbitraire, souvent 3000 dans les docs.
 
 
+const arrNames = [
+  "paul", "pierre", "mathilde"
+]
+
+
 app.get('/', (req, res) => {
   res.send('Salut tout le monde cest moi !')
 })
 // le / est la route de base. 
 
-app.get('/about', (req, res) => {
-    res.send('A propos de moi')
-  })
+app.get('/names', (req, res) => {
+
+// Une requête ne peut renvoyer qu'une seule et même réponse 
+let sentence = `${arrNames[0]} et ${arrNames[1]} et ${arrNames[2]} !`;
+  arrNames.forEach(() => {
+    res.send(sentence);
+  }
+    )
+  
+});
 
 
 app.listen(port, () => {
